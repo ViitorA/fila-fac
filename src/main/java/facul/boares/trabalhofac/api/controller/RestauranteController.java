0e29c4b6968aa195574b261020f0f7e64f188cd8
izunaa1 <izunarenovado2022@gmail.com>
2025-01-16 14:02:01 -0300

    alteracao restauranteRepository e Controller

--- src/main/java/facul/boares/trabalhofac/api/controller/RestauranteController.java
+++ src/main/java/facul/boares/trabalhofac/api/controller/RestauranteController.java
@@ -1,5 +1,7 @@
 package facul.boares.trabalhofac.api.controller;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -9,11 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 import facul.boares.trabalhofac.api.model.input.AddRestauranteDTO;
 import facul.boares.trabalhofac.api.model.input.CreateSenhaDTO;
 import facul.boares.trabalhofac.api.model.input.GerarSenhaDTO;
+import facul.boares.trabalhofac.domain.exception.PropertyNotFoundException;
+import facul.boares.trabalhofac.domain.model.Senha;
 import facul.boares.trabalhofac.domain.repository.RestauranteRepository;
 import facul.boares.trabalhofac.domain.repository.SenhaRepository;
 import facul.boares.trabalhofac.domain.service.RestauranteService;
 import facul.boares.trabalhofac.domain.service.SenhaService;
-import jakarta.el.PropertyNotFoundException;
+
 
 @RestController
 @RequestMapping("/restaurante")
@@ -24,8 +28,33 @@ public class RestauranteController {
     private SenhaRepository senhaRepository;
     private RestauranteRepository restauranteRepository;
 
-    public void estimativaTempoFila(@RequestHeader Integer numero){
-        var requestSenha = senhaRepository.findById(numero).orElseThrow(() -> new PropertyNotFoundException("Repository com esse nome não existe"));
+
+    // Colocar essa logica de codigo la no Service 
+    public float estimativaTempoFilaSenhaEspecifica(@RequestHeader Integer numero, Long restauranteId) {
+        var requestSenha = senhaRepository.findById(numero).orElseThrow(() -> new PropertyNotFoundException("Repository com esse id não existe"));
+        var requestRestaurante = restauranteRepository.findById(restauranteId).orElseThrow(() -> new PropertyNotFoundException("Repository com esse id não existe"));
+        var tempoMedioFilaSegundos = restauranteService.estimativaTempoFilaSenhaSegundos(restauranteRepository.countSenhasByRestaurante(restauranteId), 60);
+    
+        var senhaEncontrada = requestRestaurante.getSenhas().stream()
+                .filter(senha -> senha.getNumero().equals(requestSenha.getNumero()))
+                .findFirst();
+    
+        if (senhaEncontrada.isPresent()) {
+            // Lista senhas em ordem
+            List<Senha> senhasOrdenadasTempo = restauranteRepository.findSenhasByRestauranteOrderByDataCriacaoDesc(restauranteId);
+    
+            // Achar a posicao da senha na fila
+            int posicao = 1; 
+            for (Senha senha : senhasOrdenadasTempo) {
+                if (senha.getNumero().equals(numero)) {
+                    // Retorna a posicao quando encontra a senha
+                    // Fazer calculo da estimativa em minutos
+                    return tempoMedioFilaSegundos * posicao / 60;
+                }
+                posicao++;
+            }
+        }
+        throw new PropertyNotFoundException("Senha não encontrada na fila");
     }
 
     @PostMapping("/cadastrarRestaurante/")
--- src/main/java/facul/boares/trabalhofac/domain/repository/RestauranteRepository.java
+++ src/main/java/facul/boares/trabalhofac/domain/repository/RestauranteRepository.java
@@ -1,15 +1,25 @@
 package facul.boares.trabalhofac.domain.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import facul.boares.trabalhofac.domain.model.Restaurante;
+import facul.boares.trabalhofac.domain.model.Senha;
 
 @Repository
 public interface RestauranteRepository extends JpaRepository<Restaurante, Long>{
     
     Optional<Restaurante> findByNomeRestaurante(String nomeRestaurante);
+
+    @Query("SELECT s FROM Senha s WHERE s.restaurante.id = :restauranteId ORDER BY s.dataCriacao DESC")
+    List<Senha> findSenhasByRestauranteOrderByDataCriacaoDesc(@Param("restauranteId") Long restauranteId);
+
+    @Query("SELECT COUNT(s) FROM Restaurante r JOIN r.senhas s WHERE r.id = :restauranteId AND s IS NOT NULL")
+    Integer countSenhasByRestaurante(@Param("restauranteId") Long restauranteId);
 
 }
